7bbd5b0ddbb324374e88334a9fa39e5585b2d37f
Harshil Gupta <harshil.gupta@geminisolutions.com>
2022-10-18 16:09:54 +0530

    remove code documentation

--- src/main/java/com/example/project1gem/exception/CustomErrorDetail.java
+++ src/main/java/com/example/project1gem/exception/CustomErrorDetail.java
@@ -9,16 +9,8 @@ import java.util.Date;
 @AllArgsConstructor
 public class CustomErrorDetail {
 
-    /**
-     * This field is used to store time of error Occured.
-     */
+
     private Date timestamp;
-    /**
-     * This field is used to store error message.
-     */
     private String message;
-    /**
-     * This field is used to store error details.
-     */
     private String errorDetails;
 }
